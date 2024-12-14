c85c10f5e35f7aa861c20823093062b951efbf0c
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2024-12-15 01:01:31 +0800

    Delete IntroToComputing-Website-Project directory

--- IntroToComputing-Website-Project/album.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-    // Hide the loading spinner
-    document.getElementById("loading").style.display = "none";
-  
-    // Show the content
-    document.getElementById("main").style.display = "block";
-  }); 
